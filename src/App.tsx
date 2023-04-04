d98c2aae3a6eeb327fd00314dacdac19c8bceb80
Madeline <madelinejgowan@gmail.com>
2023-04-04 14:40:00 -0700

    Finally added TS 2:39PM

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container, Row, Col} from 'react-bootstrap';
+
+function App() {
+  return (
+<div>
+<Container>
+  <Row>
+    <Col>
+    </Col>
+  </Row>
+</Container>
+</div>
+  );
+}
+
+export default App;
