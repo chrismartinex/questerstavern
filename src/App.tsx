ca16a6ad02a853f0b40bb9b56528b399c565f8c8
Madeline <madelinejgowan@gmail.com>
2023-04-05 10:44:52 -0700

    10:44AM

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, {useState} from 'react';
+import {Container, Row, Col} from 'react-bootstrap';
 
 function App() {
-  return (
-    <div>
-
+  const logo = require('./Assets/logo.png');
+  const [loggedIn, setLoggedIn] = useState('')
+  const props = '';
+return(
+<div>
+      <Container>
+        <Row className='navbarBg'>
+          <Col className='col-7'>
+            <img src={logo} />
+          </Col>
+          <Col className='navMargin'>
+            <p className='doubleFont navText'>Map</p>
+          </Col>
+          <Col className='navMargin'>
+            <p className='doubleFont navText'>FAQs</p>
+          </Col>
+          <Col>
+            <p className='doubleFont navText'>D&D Website</p>
+          </Col>
+          {
+            loggedIn? (
+            <Col>
+            {/* props.username to display the active user */}
+            <p className='doubleFont navText'></p>
+            </Col>
+            ):(
+              <Col>
+              <p className='doubleFont navText'>Sign Up/Login</p>
+            </Col>
+            )
+          }
+        </Row>
+      </Container>
     </div>
-  );
+);
 }
 
 export default App;
