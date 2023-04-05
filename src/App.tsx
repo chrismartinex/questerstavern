8fb4a96073698e131564891bd6a36edca82facd2
Madeline <madelinejgowan@gmail.com>
2023-04-05 09:47:51 -0700

    Navbar Prototype 9:47AM

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,48 @@
 import './App.css';
+import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Container, Row, Col} from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
+
+  const logo = require('./Assets/logo.png');
+  const [loggedIn, setLoggedIn] = useState('')
+  const props = '';
+
   return (
-<div>
-<Container>
-  <Row>
-    <Col>
-    </Col>
-  </Row>
-</Container>
-</div>
+    <div>
+      <Container>
+        <Row className='navbarBg'>
+          <Col className='col-7'>
+            <img src={logo} />
+          </Col>
+          <Col className='navMargin'>
+            <p className='doubleFont navText'>Map</p>
+          </Col>
+          <Col className='navMargin'>
+            <p className='doubleFont navText'>FAQs</p>
+          </Col>
+          <Col>
+            <p className='doubleFont navText'>D&D Website</p>
+          </Col>
+          {
+            loggedIn? (
+            <Col>
+            {/* props.username to display the active user */}
+            <p className='doubleFont navText'></p>
+            </Col>
+            ):(
+              <Col>
+              <p className='doubleFont navText'>Sign Up/Login</p>
+            </Col>
+            )
+          }
+
+
+          
+        </Row>
+      </Container>
+    </div>
   );
 }
 
